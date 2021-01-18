1820b1e47e864fdf3f69397b7c944a90e6d8a9db
murat.ucar <murat.ucar@foriba.com>
2021-01-18 23:30:58 +0300

    Entity,Repository,Controller for Item is added.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,13 +30,14 @@ namespace Catalog.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var res= Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+            return res;
         }
     }
 }
---
+++ Entities/Item.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Catalog.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+
+}
